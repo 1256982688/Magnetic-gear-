1df93b4d5c4ba20fbf9b18d8be637be946d75c36
1256982688 <1256982688@qq.com>
2021-12-14 10:24:28 +0800

    2021.12.14

---
+++ MPI/配置/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,21 @@
+﻿#include<mpi.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#pragma warning(disable : 4996)
+
+int main(int* argc, char* argv[])
+{
+    int myid;
+    MPI_Init(NULL, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+    if (myid == 0)
+    {
+        printf("Hello!");
+    }
+    if (myid == 1)
+    {
+        printf("Hi!");
+    }
+    MPI_Finalize();
+}
